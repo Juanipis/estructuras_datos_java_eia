91fec5b6096b105dc8573d709659246c3fdeb5de
Juan Pablo Díaz Correa <juanipis@gmail.com>
2022-05-13 09:43:34 -0500

    Escritura de binarios

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,5 +1,14 @@
 package test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
@@ -8,33 +17,27 @@ import java.util.Queue;
 
 public class Main {
 	  
-    public static void main(String[] args) {
-        String numeros = "37";
-        String comparador = "307";
-        ArrayList<Integer> numerosCon = new ArrayList<>();
-        int contador = 0;
-        for (int i = 100; i <= 999; i++) {
-			if(losTiene(Integer.toString(i))) {
-				contador++;
-				numerosCon.add(i);
-			}
-		}
-        System.out.println(numerosCon);
-        System.out.println(numerosCon.size());
-        System.out.println(contador);
-    }
-    public static boolean losTiene(String msg) {
-    	boolean cond1 = false;
-    	boolean cond2 = false;
-    	for(int i = 0; i < msg.length() ; i++) {
-    		if(msg.charAt(i) == '3') {
-    			cond1 = true;
-    		}
-    		if(msg.charAt(i)== '7') {
-    			cond2 = true;
-    		}
-    	}
-    	return cond1 && cond2;
+    public static void main(String[] args) throws IOException {
+//    	Path path = Paths.get("archivobinario.dat");
+//        byte[] bytes = {110,110,101,0,0,0,111,100,111};
+//        for (int i = 0; i < bytes.length; i++) {
+//			System.out.println(bytes[i]);
+//		}
+//        try {
+//            Files.write(path, bytes);    // Java 7+ only
+//            System.out.println("Successfully written data to the file");
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
     	
+    	 FileInputStream fis = new FileInputStream(new File("archivobinario.dat"));
+    	 BufferedInputStream reader = new BufferedInputStream(fis);
+    	 int ch;
+    	 while ((ch = reader.read()) != -1) {
+    	    System.out.print(ch);
+    	 }
+    	 
     }
+    
 }
